08b78ca88670022f22695792749bcfe1c493515e
matinrezania <matin.rezania@gmail.com>
2022-05-26 20:00:32 +0430

    improve code and comments

--- UCS_search.py
+++ UCS_search.py
@@ -1,112 +1,122 @@
-matrix = [list(map(int, input().split())) for i in range(8)]
-print(matrix)
-'''
-input() takes a string as input. "1 2 3"
-split() splits the string by whitespaces and returns a
-list of strings. ["1", "2", "3"]
-list(map(int, ...)) transforms/maps the list of strings into a list of ints. [1, 2, 3]
-All these steps are done row times and these lists are stored in another list.[[1, 2, 3], [4, 5, 6], [7, 8, 9]], row = 3
-'''
+class UCSSearch:
 
-horizontal_matrix = matrix[0:4]
-vertical_matrix = matrix[4:8]
-print(horizontal_matrix)
-print(vertical_matrix)
+    def __init__(self):
+        self.current_node = {"position": {"x": 1, "y": 1},
+                             "path": [[1, 1]], "gn": 0, "parent_node": None}
+        self.queue = []
+
+    def get_input(self):
+        '''
+        input() takes a string as input. "1 2 3"
+        split() splits the string by whitespaces and returns a
+        list of strings. ["1", "2", "3"]
+        list(map(int, ...)) transforms/maps the list of strings into a list of ints. [1, 2, 3]
+        All these steps are done row times and these lists are stored in another list.[[1, 2, 3], [4, 5, 6], [7, 8, 9]], row = 3
+        '''
+        matrix = [list(map(int, input().split())) for row in range(8)]
+
+        self.horizontal_matrix = matrix[0:4]
+        self.vertical_matrix = matrix[4:8]
+
+    def sort_according_to_gn(self, item):
+        return item["gn"]
+
+    def search(self):
+        while True:
+
+            # MOVE RIGHT
+            new_node = {"position": {"x": self.current_node["position"]["x"], "y": self.current_node["position"]["y"]+1},
+                        "path": [*self.current_node["path"], [self.current_node["position"]["x"], self.current_node["position"]["y"]+1]],
+                        "gn": self.current_node["gn"]+1,
+                        "parent_node": self.current_node}
+
+            # check for constrains:(respectively) if it exceeds the grid, if it's already visited, if there's a obstacle to the right of the current node
+            if new_node["position"]["y"] != 6 and new_node not in self.queue and self.vertical_matrix[self.current_node["position"]["y"]-1][self.current_node["position"]["x"]-1] == 0:
+                # this case is only for start node, which doesn't have a parent node so no need to check if the next node is the parent of the current node
+                if not self.current_node["parent_node"]:
+                    self.queue.append(new_node)
+
+                # for all nodes other which do have parents so we should check iff the next node is the parent of the current node for each of them
+                elif new_node["position"] != self.current_node["parent_node"]["position"]:
+                    self.queue.append(new_node)
+
+            # MOVE LEFT
+            new_node = {"position": {"x": self.current_node["position"]["x"], "y": self.current_node["position"]["y"]-1},
+                        "path": [*self.current_node["path"], [self.current_node["position"]["x"], self.current_node["position"]["y"]-1]],
+                        "gn": self.current_node["gn"]+1,
+                        "parent_node": self.current_node}
+
+            # check for constrains:(respectively) if it exceeds the grid, if it's already visited, if there's a obstacle to the left of the current node
+            if new_node["position"]["y"] != 0 and new_node not in self.queue and self.vertical_matrix[self.current_node["position"]["y"]-2][self.current_node["position"]["x"]-1] == 0:
+                # this case is only for start node, which doesn't have a parent node so no need to check if the next node is the parent of the current node
+                if not self.current_node["parent_node"]:
+                    self.queue.append(new_node)
+
+                # for all nodes other which do have parents so we should check iff the next node is the parent of the current node for each of them
+                elif new_node["position"] != self.current_node["parent_node"]["position"]:
+                    self.queue.append(new_node)
+
+            # MOVE UP
+            new_node = {"position": {"x": self.current_node["position"]["x"]-1, "y": self.current_node["position"]["y"]},
+                        "path": [*self.current_node["path"], [self.current_node["position"]["x"]-1, self.current_node["position"]["y"]]],
+                        "gn": self.current_node["gn"]+3,
+                        "parent_node": self.current_node}
+
+            # check for constrains:(respectively) if it exceeds the grid, if it's already visited, if there's a obstacle to the top of the current node
+            if new_node["position"]["x"] != 0 and new_node not in self.queue and self.horizontal_matrix[self.current_node["position"]["x"]-2][self.current_node["position"]["y"]-1] == 0:
+                # this case is only for start node, which doesn't have a parent node so no need to check if the next node is the parent of the current node
+                if not self.current_node["parent_node"]:
+                    self.queue.append(new_node)
+
+                # for all nodes other which do have parents so we should check iff the next node is the parent of the current node for each of them
+                elif new_node["position"] != self.current_node["parent_node"]["position"]:
+                    self.queue.append(new_node)
+
+            # MOVE DOWN
+            new_node = {"position": {"x": self.current_node["position"]["x"]+1, "y": self.current_node["position"]["y"]},
+                        "path": [*self.current_node["path"], [self.current_node["position"]["x"]+1, self.current_node["position"]["y"]]],
+                        "gn": self.current_node["gn"]+3,
+                        "parent_node": self.current_node}
+
+            # check for constrains:(respectively) if it exceeds the grid, if it's already visited, if there's a obstacle to the bottom of the current node
+            if new_node["position"]["x"] != 6 and new_node not in self.queue and self.horizontal_matrix[self.current_node["position"]["x"]-1][self.current_node["position"]["y"]-1] == 0:
+                # this case is only for start node, which doesn't have a parent node so no need to check if the next node is the parent of the current node
+                if not self.current_node["parent_node"]:
+                    self.queue.append(new_node)
+
+                # for all nodes other which do have parents so we should check iff the next node is the parent of the current node for each of them
+                elif new_node["position"] != self.current_node["parent_node"]["position"]:
+                    self.queue.append(new_node)
+
+            # Check if there isn't any move left and yet goal is not found either so it breaks the while loop and end the search. This is the
+            # case where goal is not reachable
+            if self.queue == []:
+                break
+
+            # Sort the queue according to the g(n) of the nodes, which is the cost up to that node.
+            self.queue.sort(key=self.sort_according_to_gn)
+
+            # The following get the best choice according to UCS algorithm which is the node with the lowest cost and then delete the
+            choice = self.queue[0]
+            del self.queue[0]
+
+            # Updating the current node
+            self.current_node = choice
+
+            # GOAL TEST: If it reaches the cell with x and y equal to 5, then the search should end
+            if self.current_node["position"] == {"x": 5, "y": 5}:
+                break
+
+    def print_result(self):
+        for node in self.current_node["path"]:
+            if node == self.current_node["path"][len(self.current_node["path"])-1]:
+                print(node)
+            else:
+                print(node, end=" ")
+        print(self.current_node["gn"])
 
 
-current_node = {"position": {"x": 1, "y": 1},
-                "path": [[1, 1]], "gn": 0, "parent_node": None}
-queue_nodes = []
-
-
-def sort_queue(item):
-    # print(item)
-    return item["gn"]
-
-
-while True:
-    # move to right
-    new_node = {"position": {"x": current_node["position"]["x"], "y": current_node["position"]["y"]+1},
-                "path": [*current_node["path"], [current_node["position"]["x"], current_node["position"]["y"]+1]],
-                "gn": current_node["gn"]+1,
-                "parent_node": current_node}
-    print(new_node["position"], new_node["parent_node"]["position"])
-
-    # check wheter it can go right, check for obstacles and walls and duplicate
-    if new_node["position"]["y"] != 6 and new_node not in queue_nodes and vertical_matrix[current_node["position"]["y"]-1][current_node["position"]["x"]-1] == 0:
-        # this case is only for start node, which doesn't have a prent node
-        if not current_node["parent_node"]:
-            queue_nodes.append(new_node)
-
-        # for all nodes other than the start node
-        elif new_node["position"] != current_node["parent_node"]["position"]:
-            queue_nodes.append(new_node)
-
-    # move to left
-    new_node = {"position": {"x": current_node["position"]["x"], "y": current_node["position"]["y"]-1},
-                "path": [*current_node["path"], [current_node["position"]["x"], current_node["position"]["y"]-1]],
-                "gn": current_node["gn"]+1,
-                "parent_node": current_node}
-
-    # check wheter it can go left, check for obstacles and walls and duplicate
-    # this case is only for start node, which doesn't have a prent node
-    if new_node["position"]["y"] != 0 and new_node not in queue_nodes and vertical_matrix[current_node["position"]["y"]-2][current_node["position"]["x"]-1] == 0:
-        if not current_node["parent_node"]:
-            queue_nodes.append(new_node)
-
-        # for all nodes other than the start node
-        elif new_node["position"] != current_node["parent_node"]["position"]:
-            queue_nodes.append(new_node)
-
-    # move up
-    new_node = {"position": {"x": current_node["position"]["x"]-1, "y": current_node["position"]["y"]},
-                "path": [*current_node["path"], [current_node["position"]["x"]-1, current_node["position"]["y"]]],
-                "gn": current_node["gn"]+3,
-                "parent_node": current_node}
-
-    # check wheter it can go rigth, check for obstacles and walls and duplicate
-    if new_node["position"]["x"] != 0 and new_node not in queue_nodes and horizontal_matrix[current_node["position"]["x"]-2][current_node["position"]["y"]-1] == 0:
-        # this case is only for start node, which doesn't have a prent node
-        if not current_node["parent_node"]:
-            queue_nodes.append(new_node)
-
-        # for all nodes other than the start node
-        elif new_node["position"] != current_node["parent_node"]["position"]:
-            queue_nodes.append(new_node)
-
-    # move down
-    new_node = {"position": {"x": current_node["position"]["x"]+1, "y": current_node["position"]["y"]},
-                "path": [*current_node["path"], [current_node["position"]["x"]+1, current_node["position"]["y"]]],
-                "gn": current_node["gn"]+3,
-                "parent_node": current_node}
-
-    # check wheter it can go rigth, check for obstacles and walls and duplicate
-    # this case is only for start node, which doesn't have a prent node
-    if new_node["position"]["x"] != 6 and new_node not in queue_nodes and horizontal_matrix[current_node["position"]["x"]-1][current_node["position"]["y"]-1] == 0:
-        if not current_node["parent_node"]:
-            queue_nodes.append(new_node)
-
-        # for all nodes other than the start node
-        elif new_node["position"] != current_node["parent_node"]["position"]:
-            queue_nodes.append(new_node)
-
-    if queue_nodes == []:
-        break
-
-    queue_nodes.sort(key=sort_queue)
-
-    # to get the best choice, the choice with lowest cost
-    # print("queu is :", queue_nodes)
-    choice = queue_nodes[0]
-    del queue_nodes[0]
-    # updating the position
-    current_node = choice
-
-    # goal test case
-    if current_node["position"] == {"x": 5, "y": 5}:
-        print("goal has been reached")
-        break
-
-    # print("choice is: ", choice)
-
-print("best path: ", current_node["path"], "path cost: ", current_node["gn"])
+graph = UCSSearch()
+graph.get_input()
+graph.search()
+graph.print_result()
--- bidirectional_BFS_search.py
+++ bidirectional_BFS_search.py
@@ -1,7 +1,5 @@
-class Bidirectional_BFS_Search:
+class BidirectionalBFSSearch:
     def get_input(self):
-        matrix = [list(map(int, input().split())) for i in range(8)]
-        # print(matrix)
         '''
         input() takes a string as input. "1 2 3"
         split() splits the string by whitespaces and returns a
@@ -9,117 +7,121 @@ class Bidirectional_BFS_Search:
         list(map(int, ...)) transforms/maps the list of strings into a list of ints. [1, 2, 3]
         All these steps are done row times and these lists are stored in another list.[[1, 2, 3], [4, 5, 6], [7, 8, 9]], row = 3
         '''
+        matrix = [list(map(int, input().split())) for x in range(8)]
 
         self.horizontal_matrix = matrix[0:4]
         self.vertical_matrix = matrix[4:8]
-        # print(horizontal_matrix)
-        # print(vertical_matrix)
 
     def __init__(self):
-        self.forward_node = {"position": {"x": 1, "y": 1},
-                             "path": [[1, 1]], "gn": 0, "parent_node": None}
-        self.backward_node = {"position": {"x": 5, "y": 5},
-                              "path": [[5, 5]], "gn": 0, "parent_node": None}
+        self.forward_currnet_node = {"position": {"x": 1, "y": 1},
+                                     "path": [[1, 1]], "gn": 0, "parent_node": None}
+        self.backward_current_node = {"position": {"x": 5, "y": 5},
+                                      "path": [[5, 5]], "gn": 0, "parent_node": None}
         self.goal_found = False
         self.forward_queue = []
         self.backward_queue = []
 
-    def sort_queue(self, item):
+    def sort_function(self, item):
         return item["gn"]
 
     def BFS(self, direction):
         if not self.goal_found:
             if direction == "forward":
-                current_node = self.forward_node
+                current_node = self.forward_currnet_node
                 queue_nodes = self.forward_queue
             else:
-                current_node = self.backward_node
+                current_node = self.backward_current_node
                 queue_nodes = self.backward_queue
-            # move to right
+            # MOVE RIGHT
             new_node = {"position": {"x": current_node["position"]["x"], "y": current_node["position"]["y"]+1},
                         "path": [*current_node["path"], [current_node["position"]["x"], current_node["position"]["y"]+1]],
                         "gn": current_node["gn"]+1,
                         "parent_node": current_node}
 
-            # check wheter it can go right, check for obstacles and walls and duplicate
+            # check for constrains:(respectively) if it exceeds the grid, if it's already visited, if there's a obstacle to the right of the current node
             if new_node["position"]["y"] != 6 and new_node not in queue_nodes and self.vertical_matrix[current_node["position"]["y"]-1][current_node["position"]["x"]-1] == 0:
-                # this case is only for start node, which doesn't have a prent node
+                # this case is only for start node, which doesn't have a parent node so no need to check if the next node is the parent of the current node
                 if not current_node["parent_node"]:
                     queue_nodes.append(new_node)
 
-                # for all nodes other than the start node
+                # for all nodes other which do have parents so we should check iff the next node is the parent of the current node for each of them
                 elif new_node["position"] != current_node["parent_node"]["position"]:
                     queue_nodes.append(new_node)
-            # move to left
+            # MOVE LEFT
             new_node = {"position": {"x": current_node["position"]["x"], "y": current_node["position"]["y"]-1},
                         "path": [*current_node["path"], [current_node["position"]["x"], current_node["position"]["y"]-1]],
                         "gn": current_node["gn"]+1,
                         "parent_node": current_node}
 
-            # check wheter it can go left, check for obstacles and walls and duplicate
-            # this case is only for start node, which doesn't have a prent node
+            # check for constrains:(respectively) if it exceeds the grid, if it's already visited, if there's a obstacle to the left of the current node
             if new_node["position"]["y"] != 0 and new_node not in queue_nodes and self.vertical_matrix[current_node["position"]["y"]-2][current_node["position"]["x"]-1] == 0:
+                # this case is only for start node, which doesn't have a parent node so no need to check if the next node is the parent of the current node
                 if not current_node["parent_node"]:
                     queue_nodes.append(new_node)
 
-                # for all nodes other than the start node
+                # for all nodes other which do have parents so we should check iff the next node is the parent of the current node for each of them
                 elif new_node["position"] != current_node["parent_node"]["position"]:
                     queue_nodes.append(new_node)
 
-            # move up
+            # MOVE UP
             new_node = {"position": {"x": current_node["position"]["x"]-1, "y": current_node["position"]["y"]},
                         "path": [*current_node["path"], [current_node["position"]["x"]-1, current_node["position"]["y"]]],
                         "gn": current_node["gn"]+1,
                         "parent_node": current_node}
 
-            # check wheter it can go rigth, check for obstacles and walls and duplicate
+            # check for constrains:(respectively) if it exceeds the grid, if it's already visited, if there's a obstacle to the up of the current node
             if new_node["position"]["x"] != 0 and new_node not in queue_nodes and self.horizontal_matrix[current_node["position"]["x"]-2][current_node["position"]["y"]-1] == 0:
-                # this case is only for start node, which doesn't have a prent node
+                # this case is only for start node, which doesn't have a parent node so no need to check if the next node is the parent of the current node
                 if not current_node["parent_node"]:
                     queue_nodes.append(new_node)
 
-                # for all nodes other than the start node
+                # for all nodes other which do have parents so we should check iff the next node is the parent of the current node for each of them
                 elif new_node["position"] != current_node["parent_node"]["position"]:
                     queue_nodes.append(new_node)
 
-            # move down
+            # MOVE DOWN
             new_node = {"position": {"x": current_node["position"]["x"]+1, "y": current_node["position"]["y"]},
                         "path": [*current_node["path"], [current_node["position"]["x"]+1, current_node["position"]["y"]]],
                         "gn": current_node["gn"]+1,
                         "parent_node": current_node}
 
-            # check wheter it can go rigth, check for obstacles and walls and duplicate
-            # this case is only for start node, which doesn't have a prent node
+            # check for constrains:(respectively) if it exceeds the grid, if it's already visited, if there's a obstacle to the bottom of the current node
             if new_node["position"]["x"] != 6 and new_node not in queue_nodes and self.horizontal_matrix[current_node["position"]["x"]-1][current_node["position"]["y"]-1] == 0:
+                # this case is only for start node, which doesn't have a parent node so no need to check if the next node is the parent of the current node
                 if not current_node["parent_node"]:
                     queue_nodes.append(new_node)
 
-                # for all nodes other than the start node
+                # for all nodes other which do have parents so we should check iff the next node is the parent of the current node for each of them
                 elif new_node["position"] != current_node["parent_node"]["position"]:
                     queue_nodes.append(new_node)
 
+            # Check if there isn't any move left and yet goal is not found either so it breaks the while loop and end the search. This is the
+            # case where goal is not reachable
             if queue_nodes == []:
                 self.goal_found = True
                 print("answer not found")
 
-            # to get the best choice, the choice with lowest cost
-            # print("queu is :", queue_nodes)
-            queue_nodes.sort(key=self.sort_queue)
+            # Sort the queue according to the g(n) of the nodes, which is the cost up to that node and for every step to the right, left, up and bottom is 1
+            queue_nodes.sort(key=self.sort_function)
+
+            # The following get the best choice according to UCS algorithm which is the node with the lowest cost and then delete the
             choice = queue_nodes[0]
             del queue_nodes[0]
-            # updating the position
+
+            # Updating the current node
             current_node = choice
             if direction == "forward":
-                self.forward_node = current_node
+                self.forward_currnet_node = current_node
                 self.forward_queue = queue_nodes
             else:
-                self.backward_node = current_node
+                self.backward_current_node = current_node
                 self.backward_queue = queue_nodes
 
             self.intersect()
 
     def intersect(self):
-        if [self.backward_node["position"]["x"], self.backward_node["position"]["y"]] in self.forward_node["path"]:
+        # GOAL TEST: If it reaches the if there is a node in forward searching with the same position as a node in the backward searching, then that's a goal!
+        if [self.backward_current_node["position"]["x"], self.backward_current_node["position"]["y"]] in self.forward_currnet_node["path"]:
             self.goal_found = True
 
     def search(self):
@@ -129,18 +131,24 @@ class Bidirectional_BFS_Search:
         self.build_goal_path()
 
     def build_goal_path(self):
-        self.backward_node["path"].reverse()
-        path = [*self.forward_node["path"],
-                *self.backward_node["path"]]
+        # To find the goal path
+        self.backward_current_node["path"].reverse()
+        path = [*self.forward_currnet_node["path"],
+                *self.backward_current_node["path"]]
+        # To remove duplicate nodes in goal path
         self.goal_path = []
         [self.goal_path.append(node)
          for node in path if node not in self.goal_path]
 
+    def print_result(self):
+        for node in graph.goal_path:
+            if node == graph.goal_path[len(graph.goal_path)-1]:
+                print(node)
+            else:
+                print(node, end=" ")
 
-graph = Bidirectional_BFS_Search()
+
+graph = BidirectionalBFSSearch()
 graph.get_input()
 graph.search()
-
-print("best path:", end=" ")
-for node in graph.goal_path:
-    print(node, end=" ")
+graph.print_result()
